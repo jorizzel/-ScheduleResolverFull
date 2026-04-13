121d521fc17cb7e495a94ad7f536c74ec2a6d416
“Jorizzel” <jorizzels@gmail.com>
2026-04-13 10:40:12 +0800

    detect conflicts, rank priorities, and generate a revised schedule.

---
+++ schedule_resolver/lib/providers/schedule_provider.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../models/task_model.dart';
+import 'package:uuid/uuid.dart';
+
+class ScheduleProvider extends ChangeNotifier{
+  final List<TaskModel> _task =[]; //private only tha task model objects
+  final Uuid _uuid = const Uuid();
+
+  List<TaskModel> get task => _task;
+
+  void addTask({
+    required String title,
+    required String category,
+    required DateTime date,
+    required TimeOfDay startTime,
+    required TimeOfDay endTime,
+    required int urgency,
+    required int importance,
+    required double estimatedEffortHours,
+    required String energyLevel,
+  }) {
+    final newTask = TaskModel(
+      id: _uuid.v4(),
+      title: title,
+      category: category,
+      date: date,
+      startTime: startTime,
+      endTime: endTime,
+      urgency: urgency,
+      importance: importance,
+      estimatedEffortHours: estimatedEffortHours,
+      energyLevel: energyLevel,
+    );
+    _task.add(newTask);
+    notifyListeners();
+  }
+  void removeTask(String taskId) {
+    _task.removeWhere((task) => task.id == taskId);
+    notifyListeners();
+  }
+}
+
+
+
+
+
